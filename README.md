1ead68d5fe8059cfca7591964539f5f687162f3a
Aanand Kainth <aanand@akainth.me>
2021-05-05 12:28:59 -0700

    Add the "Inject MARS Command" command

--- README.md
+++ README.md
@@ -1,70 +1,29 @@
-# marscode README
-
-This is the README for your extension "marscode". After writing up a brief description, we recommend including the following sections.
+# marscode
 
 ## Features
+It is recommended to use this extension in conjunction with one of the many available extensions for MIPS Assembly syntax highlighting and snippets.
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+- [MIPS Support](https://marketplace.visualstudio.com/items?itemName=kdarkhan.mips)
+- [Better MIPS Support](https://marketplace.visualstudio.com/items?itemName=vasilescur.better-mips)
 
-For example if there is an image subfolder under your extension project workspace:
-
-\!\[feature X\]\(images/feature-x.png\)
-
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+When a MIPS ASM file is open, you can press `Ctrl+Shift+P` (to open the command palette) then run the Assemble and Run command.
+Alternatively, you may have a more complex use case (like passing program arguments), and for this you can open a terminal, 
+then run the "Inject MARS Command" command, which will provide a stub that you can add your program arguments / options to.
 
 ## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+Java is required to run the MARS IDE, so the JRE8 `bin` directory must be in the system `PATH` environment variable.
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
 This extension contributes the following settings:
 
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+* `marscode.marsJAR`: (optional) the path to a MARS IDE JAR file, intended for custom versions of the IDE
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+### 1.1.0
+Add the "Inject MARS command" command. That one's a mouthful.
 
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-## Following extension guidelines
-
-Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
-
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Initial release of the MARS extension
--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,65 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { exec } from "child_process";
 import { ChildProcess } from 'node:child_process';
+import { sep } from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('marscode.assembleAndRun', () => {
-
-		const filePath = vscode.window.activeTextEditor?.document.uri.fsPath;
-		if (!filePath) {
-			return;
-		}
-
-		const marsJar = vscode.workspace.getConfiguration().get("marscode.jar") as string;
-
-		const writeEmitter = new vscode.EventEmitter<string>();
-
-		let marsProc: ChildProcess;
-		const pty: vscode.Pseudoterminal = {
-			onDidWrite: writeEmitter.event,
-			open: () => {
-				marsProc = exec(`java -cp "${marsJar}" Mars "${filePath}"`);
-				marsProc.stdout?.on("data", (data: string) => {
-					writeEmitter.fire(data.replace(/\r\n|\r|\n/g, "\r\n"));
-				});
-				marsProc.on("close", console.log);
-				marsProc.on("error", console.error);
-			},
-			close: () => {
-				marsProc.kill();
-			},
-			handleInput(data) {
-				writeEmitter.fire(data.replace(/\r\n|\r|\n/g, "\r\n"));
-				marsProc.stdin?.write(data);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('marscode.termInject', () => {
+			const filePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+			if (filePath === undefined) {
+				return;
 			}
-		};
-		const terminal = vscode.window.createTerminal({ name: 'MARS', pty });
-		terminal.show();
-	});
 
-	context.subscriptions.push(disposable);
+			const marsJar =
+				vscode.workspace.getConfiguration().get("marscode.marsJAR") as string
+				|| vscode.extensions.getExtension("akainth015.marscode")!!.extensionPath + sep + "mars-4.5.jar";
+
+			vscode.window.activeTerminal?.sendText(`java -cp "${marsJar}" Mars "${filePath}"`, false);
+			vscode.window.activeTerminal?.show();
+		}),
+		vscode.commands.registerCommand('marscode.assembleAndRun', () => {
+			const filePath = vscode.window.activeTextEditor?.document.uri.fsPath;
+			if (filePath === undefined) {
+				return;
+			}
+
+			const marsJar =
+				vscode.workspace.getConfiguration().get("marscode.marsJAR") as string
+				|| vscode.extensions.getExtension("akainth015.marscode")!!.extensionPath + sep + "mars-4.5.jar";
+
+			const writeEmitter = new vscode.EventEmitter<string>();
+
+			let marsProc: ChildProcess;
+			const pty: vscode.Pseudoterminal = {
+				onDidWrite: writeEmitter.event,
+				open: () => {
+					marsProc = exec(`java -cp "${marsJar}" Mars "${filePath}"`);
+					marsProc.stdout?.on("data", (data: string) => {
+						writeEmitter.fire(data.replace(/\r\n|\r|\n/g, "\r\n"));
+					});
+					marsProc.stderr?.on("data", (data: string) => {
+						writeEmitter.fire(data);
+					});
+					marsProc.on("close", (code) => {
+						writeEmitter.fire(`Your program finished running. IDE exited with exit code ${code}.\r\n`);
+					});
+					marsProc.on("error", console.error);
+				},
+				close: () => {
+					marsProc.kill();
+				},
+				handleInput(data) {
+					if (marsProc.exitCode === null) {
+						writeEmitter.fire(data.replace(/\r\n|\r|\n/g, "\r\n"));
+						marsProc.stdin?.write(data);
+					}
+				}
+			};
+			const terminal = vscode.window.createTerminal({ name: 'MARS', pty });
+			terminal.show();
+		})
+	);
 }
 
 // this method is called when your extension is deactivated
